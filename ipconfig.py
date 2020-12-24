cc5dc464ec8dad872fe61c57b6469533dc2e6699
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2020-12-24 19:25:22 +0100

    Update ipconfig.py

--- ipconfig.py
+++ ipconfig.py
@@ -1,8 +1,8 @@
 import time
-ipv4 = 10.0.0.1
-ipv6 = none
-virtualized = yes
-version = 0.1
+ipv4 = "10.0.0.1" 
+ipv6 = "none"
+virtualized = "yes"
+version = "0.1"
 
 print("I P  C O N F I G");
 print("Version ", version)
@@ -16,6 +16,6 @@ time.sleep(0.25)
 
 print("ipv6", ipv6)
 
-time.sleep 0.5
+time.sleep(0.5)
 
 print("virtualized" virtualized)
