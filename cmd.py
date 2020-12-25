fb4d3b27ed7a72d8dbb075fafe031f8968bb0b21
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2020-12-25 07:51:45 +0000

    Create cmd.py

---
+++ cmd.py
@@ -0,0 +1,46 @@
+#it's importing hour!
+
+import time
+
+#variable setting
+
+#ipconfig.py variables
+ipv4 = "10.0.0.1" 
+ipv6 = "none"
+virtualized = "yes"
+version = "0.1"
+
+#echo.py variables
+x = input()
+echo1 = print(x)
+echo = x
+
+#It's script running time!
+print("mrchomik892's windows command line")
+print("Version: beta (rolling)")
+
+while i <= 10:
+  print("Enter command to start...")
+  
+#echo.py variable setting (cli command enter)
+
+  y = input()
+  if input y == echo
+    print('echo')
+    echo
+  elif y == ipconfig
+    print("I P  C O N F I G");
+    print("Version ", version)
+    print("loading...");
+
+    time.sleep(2)
+
+    print("ipv4", ipv4)
+
+    time.sleep(0.25)
+
+    print("ipv6", ipv6)
+
+    time.sleep(0.5)
+
+    print("virtualized", virtualized)
